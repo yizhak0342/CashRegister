6b63953a6cf5da76b5c2c977425e4faf5663de46
yitzhak mearahat <yizhak0342@gmail.com>
2021-03-02 15:53:55 +0200

    tast

--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -28,5 +28,6 @@ namespace API.Controllers
        public async Task<ActionResult<AppUser>> GetUser(int id){
        return await _context.Users.FindAsync(id);                 
         }
+        
     }
 }
